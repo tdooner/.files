1e257f0b498ba8db701b39753734194ec6b1b3fb
Tom Dooner <tomdooner@gmail.com>
2020-10-07 13:25:33 -0700

    Add starting point of script for MacOS custom preferences

---
+++ macdefaults.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# Trackpad: Force feedback, old-school scrolling
+defaults write com.apple.trackpad.forceClick -bool true
+defaults write com.apple.springing.enabled -bool true
+
+# Enable 3-finger dragging
+# TODO: Debug this and figure out why it doesn't work.
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+
+# Set a speedy key repeat interval
+defaults write NSGlobalDomain InitialKeyRepeat -int 25
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+# Iterm2: Use preferences from this repo
+defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/.files/iterm";
