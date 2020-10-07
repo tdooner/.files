a85300149e70c724403ea8e0f5a033ec6c55a9ec
Tom Dooner <tomdooner@gmail.com>
2020-10-07 15:45:22 -0700

    Compile YouCompleteMe with Python3
    This is necessary for newer versions of YCM.

--- install/ycm.sh
+++ install/ycm.sh
@@ -5,5 +5,7 @@ base_path="$HOME/.vim/bundle/YouCompleteMe"
 
 if [ ! -f "${base_path}/third_party/ycmd/ycm_core.so" ]; then
   cd ~/.vim/bundle/YouCompleteMe
-  python install.py --tern-completer
+  python3 install.py --tern-completer
+else
+  echo "YCM .so file appears to already exist!"
 fi
