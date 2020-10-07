a4be7244b09ea2473c36de38a19f151e12d9b128
Tom Dooner <tomdooner@gmail.com>
2020-10-07 15:45:10 -0700

    Add some fixes for command-t script

--- install/command-t.sh
+++ install/command-t.sh
@@ -7,18 +7,21 @@ build_command_t() {
   ruby_version=$1
   cd "$COMMAND_T_ROOT/ruby/command-t/ext/command-t"
   export PATH="/usr/local/opt/ruby/bin:${PATH}" # ruby is installed here by homebrew
-  make clean
+  if [ -f Makefile ]; then
+    make clean # if extconf.rb has been run already
+  fi
   ruby extconf.rb
   make
 }
 
 vim_ruby_link_version() {
   vim -c ':set t_ti= t_te= nomore' -c ':ruby puts "#{RUBY_VERSION}"' -cqall 2>/dev/null | \
-    grep -oE '[0-9\.]*-p[0-9]*'
+    grep -oE '[0-9]+\.[0-9]+\.[0-9]+(-p[0-9]*)?'
 }
 
 if ! vim --version | grep -q '+ruby'; then
   echo 'ERROR: Could not install command-t -- vim built without ruby'
+  exit 1
 fi
 
 # echo "Vim linked against Ruby: $ruby_link_version"
